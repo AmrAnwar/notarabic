6802dfe53887cc1f51c591eba17cc12e4fb76422
kareemhamdy500 <kareemhamdy@gimal.com>
2018-08-12 11:12:53 +0200

    model word 1 added

--- words/admin.py
+++ words/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from  .models import Word
 
 # Register your models here.
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ["created_at"]
+
+	class Meta:
+		model = Word
+
+
+
+admin.site.register(Word,PostModelAdmin)
--- words/models.py
+++ words/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Word(models.Model):
+    user        =models.ForeignKey(User,on_delete=models.CASCADE,related_name="word_creator")
+    name        = models.CharField(max_length=50,null=False)
+    description = models.TextField(null=True,blank=True)
+    example     = models.TextField(null=True,blank=True)
+    #ToDo add image field
+    up_vote     = models.IntegerField(default=0)
+    down_vote   = models.IntegerField(default=0)
+    created_at  = models.DateField(auto_now=True)
+
+
+
