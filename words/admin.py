9f4d26ef2bd561afbfaed8f18c28d689b122a01f
kareemhamdy500 <kareemhamdy@gimal.com>
2018-08-14 09:59:45 +0200

    test factory

--- words/admin.py
+++ words/admin.py
@@ -3,12 +3,19 @@ from  .models import Word, WordTimeStamp
 
 # Register your models here.
 class WordTimeStampModelAdmin(admin.ModelAdmin):
-    list_display = ["word", "today_count", "genral_count"]
+    list_display = ["word", "today_count", "genral_count", "created_at"]
+    list_editable = [ "created_at"]
 
     class Meta:
         model = WordTimeStamp
 
+class WordModel(admin.ModelAdmin):
+    list_display = ["name", "created_at"]
+
+    class Meta:
+        model = Word
 
 
-admin.site.register(Word)
+
+admin.site.register(Word, WordModel)
 admin.site.register(WordTimeStamp, WordTimeStampModelAdmin)
